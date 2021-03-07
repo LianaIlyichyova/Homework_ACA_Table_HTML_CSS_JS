e0aff65c51ed83428ebd1c9a2cc8bea1cd1f3c5f
Liana Ilyichyova <60429113+Liana-Ilyichyova-Kanakaryan@users.noreply.github.com>
2021-03-07 23:40:00 +0400

    Table

---
+++ index.js
@@ -0,0 +1,275 @@
+function absencesCount1(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button1")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter1");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount2(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button2")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter2");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount3(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button3")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter3");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount4(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button4")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter4");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount5(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button5")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter5");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount6(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button6")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter6");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount7(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button7")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter7");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount8(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button8")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter8");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount9(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button9")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter9");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount10(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button10")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter10");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount11(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button11")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter11");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount12(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button12")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter12");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount13(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button13")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter13");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount14(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button14")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter14");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+function absencesCount15(){
+   let counter=0;
+   let inputs=document.getElementsByClassName("button15")
+   for(let i=0; i<inputs.length; i++){
+      if(inputs[i].checked){
+         counter++
+      }
+   }
+   let res=document.getElementById("counter15");
+   res.innerHTML = counter;
+   if(counter>5){
+      res.style = "color:red";
+   }else{
+      res.style = "color:#D2E4E5";
+   }
+}
+
+
+
+
+
+
+
